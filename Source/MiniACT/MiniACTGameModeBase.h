91e90ee232e88995ae4a0636f8816bf931d8fae7
Hanghai Hu <14170811@qq.com>
2023-05-05 16:50:18 +0800

    create project

---
+++ Source/MiniACT/MiniACTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiniACTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIACT_API AMiniACTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
